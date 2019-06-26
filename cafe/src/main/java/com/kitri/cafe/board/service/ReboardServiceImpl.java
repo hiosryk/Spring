9495cd46a5334af30f5aab7d57ef84a16d63a26e
hiosryk <hiosryk@gmail.com>
2019-06-26 15:35:17 +0900

    2019-06-26 Cafe

--- cafe/src/main/java/com/kitri/cafe/board/service/ReboardServiceImpl.java
+++ cafe/src/main/java/com/kitri/cafe/board/service/ReboardServiceImpl.java
@@ -7,7 +7,10 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kitri.cafe.board.dao.ReboardDao;
 import com.kitri.cafe.board.model.ReboardDto;
+import com.kitri.cafe.util.CafeConstance;
+import com.kitri.cafe.util.NumberCheck;
 
 @Service
 public class ReboardServiceImpl implements ReboardService {
@@ -17,17 +20,25 @@ public class ReboardServiceImpl implements ReboardService {
 	
 	@Override
 	public int writeArticle(ReboardDto reboardDto) {
-		return 0;
+		int cnt = sqlSession.getMapper(ReboardDao.class).writeArticle(reboardDto);
+		return cnt != 0 ? reboardDto.getSeq() : 0;
 	}
 
 	@Override
 	public List<ReboardDto> listArticle(Map<String, String> parameter) {
-		return null;
+		int pg = NumberCheck.NotNumberToOne(parameter.get("pg"));
+		int end = pg * CafeConstance.ARTICLE_SIZE;
+		int start = end - CafeConstance.ARTICLE_SIZE;
+		parameter.put("start", start + "");
+		parameter.put("end", end + "");
+		return sqlSession.getMapper(ReboardDao.class).listArticle(parameter);
 	}
 
 	@Override
 	public ReboardDto viewArticle(int seq) {
-		return null;
+		ReboardDto reboardDto = sqlSession.getMapper(ReboardDao.class).viewArticle(seq);
+		reboardDto.setContent(reboardDto.getContent().replace("\n", "<br>"));
+		return reboardDto;
 	}
 
 	@Override
--- cafe/src/main/webapp/index.jsp
+++ cafe/src/main/webapp/index.jsp
@@ -1,5 +1,12 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+    pageEncoding="UTF-8" import="com.kitri.cafe.member.model.MemberDto"%>
 <%
 response.sendRedirect(request.getContextPath() + "/boardadmin/boardmenu");
+
+MemberDto memberDto = new MemberDto();
+memberDto.setId("hiosryk");
+memberDto.setName("김형섭");
+memberDto.setEmail("hiosryk@naver.com");
+
+session.setAttribute("userInfo", memberDto);
 %>
